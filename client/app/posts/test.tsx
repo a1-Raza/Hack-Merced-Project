75631e0e0caba270e827c7d739077dae01f6e09a
Abdurrahman Raza <abdurrahmanraza.ed@gmail.com>
2024-03-09 10:51:47 -0800

    Trying to create new tsx page

---
+++ client/app/posts/test.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function TestPage(){
+    return (
+        <>
+        <h1>Test text</h1>
+        <Link href="/">home</Link>
+        </>
+    );
+}
