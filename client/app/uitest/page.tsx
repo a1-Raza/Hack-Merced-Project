2abac3e8a3686342e03212aeb9c426740bb649fc
Abdurrahman Raza <abdurrahmanraza.ed@gmail.com>
2024-03-09 12:51:25 -0800

    Got different pages working, as well as Shadcn UI

---
+++ client/app/uitest/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+
+export default function UITest() {
+  return (
+    <>
+    
+    <Button variant="outline">Button</Button>
+      
+    </>
+  );
+}
