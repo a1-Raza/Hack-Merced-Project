da763999efd0c126136268596623eb8db4237fa6
John Bouyer <109704002+Turfader@users.noreply.github.com>
2024-03-09 21:00:04 -0800

    Update server.py

--- server/server.py
+++ server/server.py
@@ -1,12 +1,34 @@
-from flask import Flask, jsonify
+
+from flask import Flask, render_template, jsonify, request
+from flask_cors import CORS
+from custom_gpt import new_gpt
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/api/home", methods=["GET"])
-def return_home():
+@app.route('/')
+def index():
+    return render_template('landing.html')
+
+@app.route('/data', methods = ["GET", "POST"])
+def data():
+    if request.method == "GET":
+        return jsonify({
+            "message": "Please access this page through a post request",
+        })
+    req = dict(request.get_json())
+
+    img_url1, img_url2, text = new_gpt(36.4081, -119.0903, 0.10, "2017-05-01", "2018-09-01")
     return jsonify({
-        "message": "hello world"
+      "url1": img_url1,
+      "url2": img_url2,
+      "text": text,
     })
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+if __name__ == '__main__':
+    app.run(
+        debug=False,
+        host='0.0.0.0',
+        port=8080
+    )
